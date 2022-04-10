34e445879da37cc0a69a94e685010e53671b14da
SMaloney2017 <SMaloney2017@my.fit.edu>
2022-04-09 23:59:39 -0400

    begin implementing GraphicsEngineDisplay

---
+++ src/graphicsengine/display/GraphicsEngineDisplay.java
@@ -0,0 +1,84 @@
+package graphicsengine.display;
+
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.nio.Buffer;
+import javax.swing.JFrame;
+
+public class GraphicsEngineDisplay extends Canvas implements Runnable {
+    private JFrame renderWindow;
+    private Thread thread;
+
+    private static String title = "3DGraphicsEngine";
+    private static boolean active = false;
+
+    private static final int WIDTH = 500;
+    private static final int HEIGHT = 500;
+
+    GraphicsEngineDisplay() {
+        this.renderWindow = new JFrame();
+        this.renderWindow.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    }
+
+    public static void main(String[] args) {
+        GraphicsEngineDisplay window = new GraphicsEngineDisplay();
+        window.renderWindow.setTitle(title);
+        window.renderWindow.add(window);
+        window.renderWindow.pack();
+        window.renderWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.renderWindow.setResizable(false);
+        window.renderWindow.setVisible(true);
+        window.start();
+    }
+
+    public synchronized void start() {
+        active = true;
+        this.thread = new Thread(this, "GraphicsEngineWindow");
+        this.thread.start();
+    }
+
+    public synchronized void stop() {
+        active = false;
+        try {
+            this.thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        long lastTime = System.nanoTime();
+        final double ns = 1000000000.0 / 60.0;
+        double delta = 0;
+        while(active) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while(delta >= 1){
+                update();
+                delta--;
+            }
+            render();
+        }
+        stop();
+    }
+
+    private void render() {
+        BufferStrategy bufferStrategy = this.getBufferStrategy();
+        if(bufferStrategy == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics graphics = bufferStrategy.getDrawGraphics();
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(0, 0, WIDTH, HEIGHT);
+
+        graphics.dispose();
+        bufferStrategy.show();
+    }
+
+    private void update() {
+
+    }
+}
